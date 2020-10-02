a5b2e7ac461b04859f735b4a6ed06d75c5c58ece
trantbatey <trantbatey@gmail.com>
2020-10-02 15:22:19 -0500

    Created the spring project.

---
+++ src/main/java/com/example/myspringblog/MyspringblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyspringblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myspringblog/MyspringblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
