4408787607a0bed1b9df4cdfeb62089b52c5f405
trantbatey <trantbatey@gmail.com>
2020-10-04 09:34:18 -0500

    Completed Controllers exercise

---
+++ src/main/java/com/example/myspringblog/controllers/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.myspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "<h1>Hello from Spring!</h1>";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "<h1>Hello " + name + "!</h1>";
+    }
+}
---
+++ src/main/java/com/example/myspringblog/controllers/MathController.java
@@ -0,0 +1,43 @@
+package com.example.myspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{parm1}/and/{parm2}")
+    @ResponseBody
+    public String add(@PathVariable String parm1, @PathVariable String parm2) {
+        int num1 = Integer.parseInt(parm1);
+        int num2 = Integer.parseInt(parm2);
+        return Integer.toString(num1 + num2);
+    }
+
+    @GetMapping("/subtract/{parm1}/from/{parm2}")
+    @ResponseBody
+    public String subtract(@PathVariable String parm1, @PathVariable String parm2) {
+        int num1 = Integer.parseInt(parm1);
+        int num2 = Integer.parseInt(parm2);
+        return Integer.toString(num1 - num2);
+    }
+
+    @GetMapping("/multiply/{parm1}/by/{parm2}")
+    @ResponseBody
+    public String multiply(@PathVariable String parm1, @PathVariable String parm2) {
+        int num1 = Integer.parseInt(parm1);
+        int num2 = Integer.parseInt(parm2);
+        return Integer.toString(num1 * num2);
+    }
+
+    @GetMapping("/divide/{parm1}/by/{parm2}")
+    @ResponseBody
+    public String divide(@PathVariable String parm1, @PathVariable String parm2) {
+        int num1 = Integer.parseInt(parm1);
+        int num2 = Integer.parseInt(parm2);
+        return Integer.toString(num1 / num2);
+    }
+
+}
