6e2af437c4311c8f8fcec0f1c5ae2526c84bad82
trantbatey <trantbatey@gmail.com>
2020-10-04 09:50:14 -0500

    Renames package to springdemo

--- src/main/java/com/example/springdemo/MyspringblogApplication.java
+++ src/main/java/com/example/springdemo/MyspringblogApplication.java
@@ -1,4 +1,4 @@
-package com.example.myspringblog;
+package com.example.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/springdemo/MyspringblogApplicationTests.java
+++ src/test/java/com/example/springdemo/MyspringblogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.myspringblog;
+package com.example.springdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
