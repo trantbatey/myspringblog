31ff96758214c4c191e6d3eedb050b63d71b639c
trantbatey <trantbatey@gmail.com>
2020-10-05 11:20:11 -0500

    Made changes to PostController.

--- src/main/java/com/example/springdemo/controllers/PostController.java
+++ src/main/java/com/example/springdemo/controllers/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,19 +14,31 @@ import java.util.List;
 @Controller
 public class PostController {
 
-    @GetMapping("/post/{title}")
-    public String showPost(@PathVariable String title, Model model) {
-        Post post = new Post("A Single Post", "This is the body of a single test post.");
-        model.addAttribute("post", post);
-        return "posts/post";
-    }
-
-    @GetMapping("/posts")
+    @GetMapping("/post")
     public String showPosts(Model model) {
         List<Post> postList = new ArrayList<>();
         postList.add(new Post("First Post", "This is the body of the first test post."));
         postList.add(new Post("Second Post", "This is the body of the second test post."));
         model.addAttribute("postList", postList);
         return "posts/list";
+    }
+
+    @GetMapping("/post/{id}")
+    public String showPost(@PathVariable Integer id, Model model) {
+        Post post = new Post("A Single Post", "This is the body of a single test post.");
+        model.addAttribute("post", post);
+        return "posts/post";
+    }
+
+    @GetMapping("/post/create")
+    @ResponseBody
+    public String showCreatePost () {
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "create a new post";
     }
 }
